70e7cdcb57fbff398d4890cdf1fc95b94663b733
築地里桜 <oreoandrio@gmail.com>
2022-03-21 22:22:33 +0900

    a

--- src/App.tsx
+++ src/App.tsx
@@ -10,21 +10,6 @@ function App() {
 
     const number = Math.floor(Math.random()*2);
 
-    const [correct, setCorrect] = useState(false);
-    const [visible, setVisible] = useState(false);
-    const [last, setLast] = useState(false);
-    const [count, setCount] = useState(0);
-    const [correctCount, setCorrectCount] = useState(0);
-    //const [questions] = useState<Problem[]>(route.params.props);
-
-    const checkAnswer = (flag: boolean) => {
-        if (flag) {
-            setCorrect(true);
-        } else {
-            setCorrect(false);
-        }
-    }
-
   return (
     <div className="App">
             <Question number={number}/>
